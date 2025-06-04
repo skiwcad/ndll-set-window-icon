8a20855cf92959c10d3dfc01027cefc603dd10c8
skiwcad <skiwcad@proton.me>
2025-06-03 22:21:48 -0400

    lots of small changes
    change parameter count
    make error messages red and more visible in the console
    rename hIconSmall and hIconLarge to iconSmall and iconLarge
    use frontslashes for the paths
    add an error message when attempting to use on non windows platforms
    remove commented out code
    new readme
    formatting

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # Set Window Icon NDLL
 
 Forked from the [Codename Engine NDLL Example](https://github.com/CodenameCrew/ndll-example)
+
+This NDLL will only work on Windows.
+
+## Usage in Codename:
+
+```hx
+import funkin.backend.utils.NdllUtil;
+
+// place ndll in mods/yourmodname/ndlls/
+
+var setWindowIcon = NdllUtil.getFunction('set_window_icon', "set_window_icon", 2);
+// setWindowIcon(path:String, modsFolder:String);
+// if modsFolder is a string, it will prefix the path with "./mods/" + modsFolder + "/"
+
+// setting icon to custom icon located in ./mods/yourmodname/icon.ico
+#if windows
+setWindowIcon('icon.ico', ModsFolder.currentModFolder);
+#end
+
+// setting icon to default icon.ico located in ./icon.ico
+#if windows
+setWindowIcon('icon.ico', null);
+#end
+```
--- project/common/ExternalInterface.cpp
+++ project/common/ExternalInterface.cpp
@@ -13,69 +13,61 @@
 #pragma comment(lib, "Dwmapi")
 #endif
 
-#include <iostream>
-#include <string>
-
-//#include <hxcpp.h>
 #include <hx/CFFI.h>
 #include <hx/CFFIPrime.h>
 
-#if defined(HX_WINDOWS)
+#include <string>
+#include <iostream>
 
 using namespace std;
 
-// returns true when successful, an error message otherwise.
-static value set_window_icon(value hpath, bool mods, value hmodsFolder) {
-	if (!val_is_string(hpath) || !val_is_string(hmodsFolder)) {
-		cerr << "Bad arguments!" << endl;
+#if defined(HX_WINDOWS)
+
+static value set_window_icon(value hpath, value hmodsFolder) {
+	if (!val_is_string(hpath)) {
+		cerr << "\x1b[31m[ERROR] Icon path must be a string\x1b[0m" << endl;
 		return alloc_bool(false);
 	}
 
-	string path = string(val_string(hpath));
-	string iconPath = ".\\" + path;
+	const string path = string(val_string(hpath));
 
-	if (mods) {
+	string iconPath;
+	if (val_is_string(hmodsFolder)) {
 		string modsFolder = string(val_string(hmodsFolder));
-		iconPath = ".\\mods\\" + modsFolder + "\\" + path;
-	}
-
-	HWND hWnd = GetActiveWindow();
-	HICON hIconSmall = (HICON) LoadImage(NULL, iconPath.c_str(), IMAGE_ICON, 16, 16, LR_LOADFROMFILE);
-	HICON hIconLarge = (HICON) LoadImage(NULL, iconPath.c_str(), IMAGE_ICON, 256, 256, LR_LOADFROMFILE);
-
-	if (!hIconLarge || !hIconSmall) {
-    	cerr << "Failed to load icons. Error code: " << GetLastError() << endl;
-		cerr << "Icon path: " << iconPath << endl;
-		return alloc_bool(false);
+		iconPath = "./mods/" + modsFolder + "/" + path;
 	} else {
-		SendMessage(hWnd, WM_SETICON, ICON_SMALL, (LPARAM) hIconSmall);
-		SendMessage(hWnd, WM_SETICON, ICON_BIG, (LPARAM) hIconLarge);
+		iconPath = "./" + path;
 	}
+
+	const HWND windowHandle = GetActiveWindow();
+	const HICON iconSmall = (HICON) LoadImage(NULL, iconPath.c_str(), IMAGE_ICON, 16, 16, LR_LOADFROMFILE);
+	const HICON iconBig = (HICON) LoadImage(NULL, iconPath.c_str(), IMAGE_ICON, 256, 256, LR_LOADFROMFILE);
+
+	if (!iconBig || !iconSmall) {
+    	cerr << "\x1b[31m[ERROR] Failed to load icons from: " << iconPath <<
+			" (Error code: " << GetLastError() << ")\x1b[0m" << endl;
+		return alloc_bool(false);
+	}
+
+	SendMessage(windowHandle, WM_SETICON, ICON_SMALL, (LPARAM) iconSmall);
+	SendMessage(windowHandle, WM_SETICON, ICON_BIG, (LPARAM) iconBig);
 
 	return alloc_bool(true);
 }
-DEFINE_PRIME3(set_window_icon);
+DEFINE_PRIME2(set_window_icon);
 
 #else
 
 // doesnt work on non-windows platforms
-static value set_window_icon(value modsFolder) {
+static value set_window_icon(value hpath, value hmodsFolder) {
+	cerr << "\x1b[31m[ERROR] set_window_icon is only available for Windows\x1b[0m" << endl;
 	return alloc_bool(false);
 }
-DEFINE_PRIME1(set_window_icon);
+DEFINE_PRIME2(set_window_icon);
 
 #endif
 
-// if someone can tell me how to get a variable from a haxe class i will be forever grateful
-/*static value test() {
-	hx::Class folder = hx::Class_obj::Resolve(HX_CSTRING("funkin.backend.assets.ModsFolder"));
-	hx::Val val = folder->__Field(HX_CSTRING("currentModFolder"), false);
-	char const * modsFolder = val->toString().__CStr();
-	return alloc_string(modsFolder);
-}
-DEFINE_PRIME0(test);*/
-
 extern "C" void ndll_main() {
-	val_int(0); // Fix Neko init
+	val_int(0); // fix neko init
 }
 DEFINE_ENTRY_POINT(ndll_main);
